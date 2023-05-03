b60f3a0c384560554dca9824f06905a7b2693cdb
Stamencho Bogdanovski <u11653@asambeauty.com>
2023-05-03 18:57:35 +0200

    Initial commit

---
+++ src/main/java/com/example/student_mgmt_system/StudentMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_mgmt_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/student_mgmt_system/StudentMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_mgmt_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
